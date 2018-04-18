51b2e85f7d9fd8e3b76df8a6e46e5c2d93605bf3
Jeremy Morrell <jmorrell@heroku.com>
2018-04-18 14:28:58 -0700

    No need to check node version at runtime

--- .profile.d/heroku-nodejs-metrics-plugin.sh
+++ .profile.d/heroku-nodejs-metrics-plugin.sh
@@ -5,16 +5,4 @@ if [[ -z "$HEROKU_METRICS_URL" ]] || [[ "${DYNO}" = run\.* ]]; then
     return 0
 fi
 
-skip_old_node_versions() {
-    local node_version="$(node --version)"
-    if [ "${node_version:1:1}" -lt "8" ] || [ "${node_version:1:2}" -ne "10" ]; then
-        echo "The Heroku Node.js Metrics Plugin does not support Node v${node_version}."
-        echo "No Node-specific metrics will be collected for this application until it is upgraded."
-        echo ""
-        echo "Read more: https://devcenter.heroku.com/articles/language-runtime-metrics-nodejs"
-        exit 0
-    fi
-}
-
-skip_old_node_versions
 export NODE_OPTIONS="--require $HOME/.heroku/node-metrics-plugin"
