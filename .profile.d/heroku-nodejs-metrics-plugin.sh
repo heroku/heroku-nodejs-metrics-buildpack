cd271f718dbdcc09a682a348bca0bde2548f0d60
Jeremy Morrell <morrell.jeremy@gmail.com>
2018-04-18 16:20:35 -0700

    Do not overwrite NODE_OPTIONS if it is already set, append (#5)

--- .profile.d/heroku-nodejs-metrics-plugin.sh
+++ .profile.d/heroku-nodejs-metrics-plugin.sh
@@ -5,4 +5,8 @@ if [[ -z "$HEROKU_METRICS_URL" ]] || [[ "${DYNO}" = run\.* ]]; then
     return 0
 fi
 
-export NODE_OPTIONS="--require $HOME/.heroku/node-metrics-plugin"
+if [[ -z "$NODE_OPTIONS" ]]; then
+    export NODE_OPTIONS="--require $HOME/.heroku/node-metrics-plugin"
+else
+    export NODE_OPTIONS="${NODE_OPTIONS} --require $HOME/.heroku/node-metrics-plugin"
+fi
