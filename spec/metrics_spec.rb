e1005d88a41fddb52853eaabe4078b10625168d2
Jeremy Morrell <morrell.jeremy@gmail.com>
2018-04-18 15:12:03 -0700

    Don't include the plugin for unsupported Node versions (#4)
    Don't include the plugin for unsupported Node versions

--- spec/metrics_spec.rb
+++ spec/metrics_spec.rb
@@ -1,12 +1,13 @@
 require_relative 'spec_helper'
 require 'json'
 
+branch = ENV['HATCHET_BUILDPACK_BRANCH'] || "master"
+
 describe "Node Metrics Hello World" do
 
   before(:each) do
     set_node_version(app.directory, node_version)
     app.setup!
-    # `heroku features:enable runtime-heroku-metrics --app #{app.name}`
     app.set_config({
       "HEROKU_METRICS_URL" => "http://localhost:3000",
       "METRICS_INTERVAL_OVERRIDE" => "10000"
@@ -24,7 +25,7 @@ describe "Node Metrics Hello World" do
       let(:app) {
         Hatchet::Runner.new(
           "node-metrics-single-process",
-          buildpacks: ["heroku/nodejs", "https://github.com/heroku/heroku-nodejs-metrics-buildpack.git"]
+          buildpacks: ["heroku/nodejs", "https://github.com/heroku/heroku-nodejs-metrics-buildpack.git##{branch}"]
         )
       }
       let(:node_version) { version }
@@ -43,7 +44,7 @@ describe "Node Metrics Hello World" do
       let(:app) {
         Hatchet::Runner.new(
           "node-metrics-multi-process",
-          buildpacks: ["heroku/nodejs", "https://github.com/heroku/heroku-nodejs-metrics-buildpack.git"]
+          buildpacks: ["heroku/nodejs", "https://github.com/heroku/heroku-nodejs-metrics-buildpack.git##{branch}"]
         )
       }
       let(:node_version) { version }
@@ -80,7 +81,7 @@ describe "Node Metrics" do
       let(:app) {
         Hatchet::Runner.new(
           "node-metrics-test-app",
-          buildpacks: ["heroku/nodejs", "https://github.com/heroku/heroku-nodejs-metrics-buildpack.git"]
+          buildpacks: ["heroku/nodejs", "https://github.com/heroku/heroku-nodejs-metrics-buildpack.git##{branch}"]
         )
       }
 
@@ -98,5 +99,37 @@ describe "Node Metrics" do
 
 end
 
+describe "Unsupported Node version" do 
 
+  before(:each) do
+    set_node_version(app.directory, node_version)
+    app.setup!
+    app.set_config({
+      "HEROKU_METRICS_URL" => "http://localhost:3000",
+      "METRICS_INTERVAL_OVERRIDE" => "10000"
+    })
+  end
+
+  ["6.0.0"].each do |version|
+    context "Unsupported Node v#{version} app" do
+      let(:app) {
+        Hatchet::Runner.new(
+          "node-metrics-single-process",
+          buildpacks: ["heroku/nodejs", "https://github.com/heroku/heroku-nodejs-metrics-buildpack.git##{branch}"]
+        )
+      }
+
+      let(:node_version) { version }
+      it "should deploy" do
+        app.deploy do |app|
+          expect(app.output).to include("-----> Build succeeded!")
+          expect(app.output).to include("HerokuNodejsRuntimeMetrics app detected")
+          expect(app.output).to include("The Heroku Node.js Metrics Plugin does not support Node v#{version}")
+          expect(app.output).to include("https://devcenter.heroku.com/articles/language-runtime-metrics-nodejs")
+          expect(successful_body(app).strip).to eq("Hello, world!")
+        end
+      end
+    end
+  end
+end
 
