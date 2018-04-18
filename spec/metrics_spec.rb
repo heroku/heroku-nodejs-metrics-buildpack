564d070a6665d5cb202c29bd31abe8f942122186
Jeremy Morrell <jmorrell@heroku.com>
2018-04-18 14:11:30 -0700

    Dont include the plugin for unsupported Node versions

--- spec/metrics_spec.rb
+++ spec/metrics_spec.rb
@@ -98,5 +98,37 @@ describe "Node Metrics" do
 
 end
 
+describe "Unsupported Node version" do 
 
+  before(:each) do
+    set_node_version(app.directory, node_version)
+    app.setup!
+    app.set_config({
+      "HEROKU_METRICS_URL" => "http://localhost:3000",
+      "METRICS_INTERVAL_OVERRIDE" => "10000"
+    })
+  end
+
+  ["6.0.0"].each do |version|
+    context "Unsupported Node v#{version} app" do
+      let(:app) {
+        Hatchet::Runner.new(
+          "node-metrics-single-process",
+          buildpacks: ["heroku/nodejs", "https://github.com/heroku/heroku-nodejs-metrics-buildpack.git"]
+        )
+      }
+
+      let(:node_version) { version }
+      it "should deploy" do
+        app.deploy do |app|
+          expect(app.output).to include("-----> Build succeeded!")
+          expect(app.output).to include("HerokuNodejsRuntimeMetrics app detected")
+          expect(app.output).to include("The Heroku Node.js Metrics Plugin does not support Node v#{version}")
+          expect(app.output).to include("https://devcenter.heroku.com/articles/language-runtime-metrics-nodejs")
+          expect(successful_body(app).strip).to eq("Hello, world!")
+        end
+      end
+    end
+  end
+end
 
