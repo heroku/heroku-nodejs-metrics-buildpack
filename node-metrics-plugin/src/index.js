684131fa6c7a549d250336df332ee8d252174451
Jeremy Morrell <morrell.jeremy@gmail.com>
2018-05-30 13:52:15 -0700

    Remove gc-stats so that the build will pass with Node 10.x (#19)
    Remove gc-stats so that the build will pass with Node 10.x

--- node-metrics-plugin/src/index.js
+++ node-metrics-plugin/src/index.js
@@ -1,40 +1,46 @@
-const gc = require("gc-stats")();
-const request = require("request");
+const http = require('http');
+const url = require('url');
 const { Histogram } = require("measured");
-const eventLoopStats = require('./eventLoopStats');
+const nativeStats = require('./nativeStats');
 
 const METRICS_INTERVAL = parseInt(process.env.METRICS_INTERVAL_OVERRIDE, 10) || 20000; // 20 seconds
-
-// Add timers that approximate the delay in the event loop
-// const eventLoopMonitor = new EventLoopMonitor();
-// eventLoopMonitor.start(EVENT_LOOP_INTERVAL);
-
-// pauseNS is the cumulative GC pause of the node program between GC runs.
-// This is reset every metrics submission run.
-let pauseNS = 0;
-
-// gcCount is the number of garbage collections that have been observed between
-// metrics runs. This is reset every metrics submission run.
-let gcCount = 0;
 
 // Collects the event loop ticks, and calculates p50, p95, p99, max
 let delay = new Histogram();
 
-// metricsURL is where the runtime metrics will be posted to. This is added
+// url is where the runtime metrics will be posted to. This is added
 // to dynos by runtime iff the app is opped into the heroku runtime metrics
 // beta.
-const metricsURL = process.env.HEROKU_METRICS_URL;
+let uri = url.parse(process.env.HEROKU_METRICS_URL);
 
-// on every garbage collection, update the statistics.
-gc.on("stats", stats => {
-  gcCount++;
+nativeStats.start();
 
-  pauseNS = pauseNS + stats.pause;
-});
+function submitData(data, cb) {
+  const postData = JSON.stringify(data);
 
-// every 20 seconds, submit a metrics payload to metricsURL.
+  // post data to metricsURL
+  const options = {
+    method: "POST",
+    hostname: uri.hostname,
+    port: uri.port,
+    path: uri.path,
+    headers: {
+      "Content-Type": "application/json",
+      "Content-Length": Buffer.byteLength(postData),
+    },
+  };
+
+  const req = http.request(options, res => {
+    cb(null, res);
+  });
+  req.on('error', cb);
+  req.write(postData);
+  req.end();
+}
+
+// every METRICS_INTERVAL seconds, submit a metrics payload to metricsURL.
 setInterval(() => {
-  let ticks = eventLoopStats.sense();
+  let { ticks, gcCount, gcTime } = nativeStats.sense();
   let totalEventLoopTime = ticks.reduce((a, b) => a + b, 0);
 
   ticks.forEach(tick => delay.update(tick));
@@ -43,10 +49,10 @@ setInterval(() => {
 
   let { median, p95, p99, max } = delay.toJSON();
 
-  data = {
+  let data = {
     counters: {
       "node.gc.collections": gcCount,
-      "node.gc.pause.ns": pauseNS
+      "node.gc.pause.ns": gcTime,
     },
     gauges: {
       "node.eventloop.usage.percent": aa,
@@ -57,36 +63,23 @@ setInterval(() => {
     }
   };
 
-  // post data to metricsURL
-  options = {
-    method: "POST",
-    uri: metricsURL,
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-  };
-
-  request(options, (error, resp, body) => {
-    if (error !== null) {
+  submitData(data, (err, res) => {
+    if (err !== null) {
       console.log(
-        "[fuzzy-rotary-phone] error when trying to submit data: ",
-        error
+        "[heroku-nodejs-plugin] error when trying to submit data: ",
+        err
       );
       return;
     }
 
-    if (resp.statusCode !== 200) {
+    if (res.statusCode !== 200) {
       console.log(
-        "[fuzzy-rotary-phone] expected 200 when trying to submit data, got:",
-        resp.statusCode
+        "[heroku-nodejs-plugin] expected 200 when trying to submit data, got:",
+        res.statusCode
       );
-      console.log("[fuzzy-rotary-phone] body:", body);
       return;
     }
   });
 
-  pauseNS = 0;
-  gcCount = 0;
   delay.reset();
 }, METRICS_INTERVAL).unref();
--- spec/metrics_spec.rb
+++ spec/metrics_spec.rb
@@ -3,6 +3,9 @@ require 'json'
 
 branch = ENV['HATCHET_BUILDPACK_BRANCH'] || "master"
 
+# List of "bad" versions of Node
+version_blacklist = ["10.2.0"]
+
 describe "Node Metrics Hello World" do
 
   before(:each) do
@@ -17,7 +20,7 @@ describe "Node Metrics Hello World" do
   if ENV['TEST_ALL_NODE_VERSIONS'] == 'true'
     versions = resolve_all_supported_node_versions()
   else 
-    versions = resolve_node_version(["8.x", "9.x"])
+    versions = resolve_node_version(["8.x", "9.x", "10.x"])
   end
 
   versions.each do |version|
@@ -33,7 +36,11 @@ describe "Node Metrics Hello World" do
         app.deploy do |app|
           expect(app.output).to include("-----> Build succeeded!")
           expect(app.output).to include("HerokuNodejsRuntimeMetrics app detected")
-          expect(successful_body(app).strip).to eq("--require /app/.heroku/node-metrics-plugin")
+          if version_blacklist.include? version
+            expect(successful_body(app).strip).to eq("not found")
+          else
+            expect(successful_body(app).strip).to eq("--require /app/.heroku/node-metrics-plugin")
+          end
         end
       end
     end
@@ -73,11 +80,15 @@ describe "Node Metrics" do
   if ENV['TEST_ALL_NODE_VERSIONS'] == 'true'
     versions = resolve_all_supported_node_versions()
   else 
-    versions = resolve_node_version(["8.x", "9.x", "10.1.x"])
+    versions = resolve_node_version(["8.x", "9.x", "10.x"])
   end
 
   versions.each do |version|
+    # Skip tests if this version is in the blacklist
+    next if version_blacklist.include? version
+
     context "a multi-process node v#{version} app" do
+
       let(:app) {
         Hatchet::Runner.new(
           "node-metrics-test-app",
@@ -96,7 +107,6 @@ describe "Node Metrics" do
       end
     end
   end
-
 end
 
 describe "Unsupported Node version" do 
@@ -160,6 +170,39 @@ describe "Don't overwrite NODE_OPTIONS" do
           expect(app.output).to include("HerokuNodejsRuntimeMetrics app detected")
           expect(app.output).to include("NODE_OPTIONS: --max-old-space-size=500")
           expect(successful_body(app).strip).to eq("--max-old-space-size=500 --require /app/.heroku/node-metrics-plugin")
+        end
+      end
+    end
+  end
+end
+
+describe "Unsupported Node version" do 
+
+  before(:each) do
+    set_node_version(app.directory, node_version)
+    app.setup!
+    app.set_config({
+      "HEROKU_METRICS_URL" => "http://localhost:3000",
+      "METRICS_INTERVAL_OVERRIDE" => "10000"
+    })
+  end
+
+  ["10.2.0"].each do |version|
+    context "Blacklisted Node v#{version} app" do
+      let(:app) {
+        Hatchet::Runner.new(
+          "node-metrics-multi-process",
+          buildpacks: ["heroku/nodejs", "https://github.com/heroku/heroku-nodejs-metrics-buildpack.git##{branch}"]
+        )
+      }
+
+      let(:node_version) { version }
+      it "should deploy" do
+        app.deploy do |app|
+          expect(app.output).to include("-----> Build succeeded!")
+          expect(app.output).to include("HerokuNodejsRuntimeMetrics app detected")
+          expect(app.output).to include("Node v10.2.0 is not supported by the Heroku Metrics plugin")
+          expect(successful_body(app).strip).to eq("Hello, world!")
         end
       end
     end
