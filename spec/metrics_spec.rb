4b31cb6833dae3dd5ed57356532b5e590b4ede52
Jeremy Morrell <jmorrell@heroku.com>
2018-08-16 17:01:09 -0700

    Missed one

--- spec/metrics_spec.rb
+++ spec/metrics_spec.rb
@@ -83,7 +83,7 @@ describe "Node Metrics" do
   elsif ENV['TEST_ALL_NODE_VERSIONS'] == 'true'
     versions = resolve_all_supported_node_versions()
   else 
-    versions = resolve_node_version(["8.x", "9.x", "10.x"])
+    versions = resolve_node_version(["6.x", "8.x", "9.x", "10.x"])
   end
 
   versions.each do |version|
